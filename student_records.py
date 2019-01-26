8086b2e32b86ab73421bb0cb2c549d0b5b807477
Saran Sappa <46707036+saransappa@users.noreply.github.com>
2019-01-26 16:24:54 +0530

    Add files via upload

---
+++ student_records.py
@@ -0,0 +1,20 @@
+import ast
+def record():
+	d=ast.literal_eval(input())
+	#s=s.replace(":","=")
+	#d=dict(s)
+	
+	a=[]
+	for i in d.values():
+		a.append(i)
+	a.sort()
+	a.reverse()
+	m=len(a)
+	i=0
+	for k,v in d.items():
+		d[k]=a[i]
+		i+=1
+	print(d)
+	return
+
+record()
